2a5ec3edbc34eecdd10b793c3025032b0d6211a6
Riham Mohamed <moriham190@gmail.com>
2026-06-27 00:21:16 +0200

    Fix student dashboard and enrollment issue

--- src/app/features/student/components/student-assignments/student-assignments.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-student-assignments',
-  imports: [],
-  templateUrl: './student-assignments.html',
-  styleUrl: './student-assignments.css',
-})
-export class StudentAssignments {}
